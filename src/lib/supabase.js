01c9028a0136732142cba14f3820c41ab08359d3
albertoloborei-gif <albertoloborei@gmail.com>
2026-06-17 20:59:47 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://clnanmrmcedlsxvjlily.supabase.co'
+
+const supabaseAnonKey = 'COLE_AQUI_SUA_CHAVE_PUBLICAVEL_COMPLETA'
 
 export const supabase = createClient(
   supabaseUrl,
