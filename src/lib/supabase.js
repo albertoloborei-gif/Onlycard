a743798223e2a9be4cfda4fc2c06b9eb502c9b53
albertoloborei-gif <albertoloborei@gmail.com>
2026-06-18 22:39:32 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://clnanmrmcedlsxvjlily.supabase.co'
+console.log('SUPABASE URL:', 'https://clnanmrmcedlsxvjlily.supabase.co')
+console.log('SUPABASE KEY:', 'sb_publishable__oMuzHDivEpEaRVG0tKlGQ_N4Dsp3mv')
 
+const supabaseUrl = 'https://clnanmrmcedlsxvjlily.supabase.co'
 const supabaseAnonKey = 'sb_publishable__oMuzHDivEpEaRVG0tKlGQ_N4Dsp3mv'
 
 export const supabase = createClient(
